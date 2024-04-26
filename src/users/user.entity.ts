fcde93467658f1b3fea68b82d0588e2859b5bc27
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2024-04-26 17:34:26 -0500

    added users

---
+++ src/users/user.entity.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity( name: 'users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id_user: number;
+
+    @Column()
+    firstname_user: string;
+
+    @Column()
+    lastname_user: string;
+
+    @Column()
+    email_user: string;
+
+    @Column()
+    phone_user: string;
+
+    @Column()
+    image_user: string;
+
+    @Column()
+    password_user: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    created_at: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    updated_at: string;
+
+}
